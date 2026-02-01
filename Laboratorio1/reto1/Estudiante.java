1371248079212f1a85c5bc2963a675495e4e979f
Rafael_Moreno <rafael.moreno@giitic.com>
2026-02-01 11:07:54 -0500

    feat: Agrego clase Estudiante

---
+++ Laboratorio1/reto1/Estudiante.java
@@ -0,0 +1,13 @@
+public class Estudiante {
+    String nombre;
+    int edad;
+    String correo;
+    int semestre;
+
+    public Estudiante(String nombre, int edad, String correo, int semestre) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+        this.semestre = semestre;
+    }
+}
