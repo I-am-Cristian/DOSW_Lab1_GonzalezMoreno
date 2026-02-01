ca3b58aeb3a04dad8da606e3f2d4b9b7250d4419
Rafael_Moreno <rafael.moreno@giitic.com>
2026-02-01 11:46:56 -0500

    feat: renombra Reto2 a CarreraParalela y crea base

---
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class CarreraParalela {
+
+    // Aquí luego se unifica todo
+    public static void main(String[] args) {
+        System.out.println("Carrera Paralela - punto de partida");
+    }
+}
